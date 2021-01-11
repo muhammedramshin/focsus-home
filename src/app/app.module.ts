afd85a7ec2938a93bfab09c94fc1f328d4e92a41
muhammed ramshin <muhammedramshin@gmail.com>
2021-01-11 19:00:55 +0530

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HeaderComponent } from './components/header/header.component';
 import { FeaturesComponent } from './components/features/features.component';
+import { NgImageSliderModule } from 'ng-image-slider';
 
+import { SliderModule } from 'angular-image-slider';
+import { FooterComponent } from './components/footer/footer.component';
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     HeaderComponent,
-    FeaturesComponent
+    FeaturesComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    SliderModule,
+    BrowserAnimationsModule,
+    NgImageSliderModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,11 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  imageObject = [{
+    image: '../../../assets/shot1.png',
+    thumbImage: '../../../assets/shot1.png',
+    
+}, {
+  image: '../../../assets/shot1.png',
+  thumbImage: '../../../assets/shot2.png',
+}, {
+  image: '../../../assets/shot1.png',
+  thumbImage: '../../../assets/shot3.png',
+    
+},{
+    image: '../../../assets/shot1.png',
+    thumbImage: '../../../assets/shot4.png',
+    
+}, {
+  image: '../../../assets/shot1.png',
+  thumbImage: '../../../assets/shot5.png',
+}];
 
   constructor() { }
 
   ngOnInit(): void {
+    
+
   }
+ 
 
  
 }
